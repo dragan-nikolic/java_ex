74b200730921d542ce8a1995e202884c0d74752a
Dragan Nikolic <dragan@fintechdev.net>
2017-03-11 20:18:41 -0800

    add hello world

---
+++ hello.java
@@ -0,0 +1,15 @@
+/*
+This is Hello World java app :)
+
+Build:
+javac <file>.java
+Run:
+java <file>
+*/
+
+// name of the class must be same as name of the file
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World by Dragan!");
+    }
+}
