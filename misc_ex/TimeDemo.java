2872552285803fd2cde9a033c8d71425bdf08f94
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2019-07-29 18:05:18 +0200

    more date examples

--- misc_ex/TimeDemo.java
+++ misc_ex/TimeDemo.java
@@ -1,13 +1,14 @@
 /*
-java string example
-
-Here is excellent tutorial: https://www.tutorialspoint.com/java/java_strings.htm
-
-The String class has 11 constructors that allow you to provide the initial value 
-of the string using different sources, such as an array of characters.
+java Time and Date examples
 */
+
+
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class TimeDemo {
     public static void TodayInFormat() {
@@ -16,7 +17,21 @@ public class TimeDemo {
         System.out.println(dateString);
     }
 
+    public static void AddDaysToDate() {
+        SimpleDateFormat format = new SimpleDateFormat("dd-MM-YYYY");
+        Date currentDate = new Date();
+        System.out.println(format.format(currentDate));
+        LocalDateTime ldt = currentDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        LocalDateTime ldtm3 = ldt.minusDays(3);       
+        Date currentMinus3 = Date.from(ldtm3.atZone(ZoneId.systemDefault()).toInstant());
+        System.out.println(format.format(currentMinus3));
+        LocalDateTime ldtp3 = ldt.plusDays(3);       
+        Date currentPlus3 = Date.from(ldtp3.atZone(ZoneId.systemDefault()).toInstant());
+        System.out.println(format.format(currentPlus3));
+    }
+
     public static void main(String args[]) {
         TodayInFormat();
+        AddDaysToDate();
     }
 }
