487a4a44fe3132dd23b3ed4b9f616aacb6d016f4
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2019-12-01 11:30:08 +0100

    complete producer consumer :)

--- courses/pluralsight/cocurency-and-multi-threading/02/producer_consumer/src/main/java/waitnotify/ProducerConsumer.java
+++ courses/pluralsight/cocurency-and-multi-threading/02/producer_consumer/src/main/java/waitnotify/ProducerConsumer.java
@@ -2,24 +2,46 @@ package waitnotify;
 
 public class ProducerConsumer {
 
+    private static Object lock = new Object();
+
     private static int[] buffer;
     private static int count;
 
     static class Producer {
 
         void produce() {
-            while (isFull(buffer)) {}
-            buffer[count++] = 1;
-            printBuffer("Produce");
+            synchronized(lock) {
+                if (isFull(buffer)) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                buffer[count++] = 1;
+                printBuffer("Produce");
+                lock.notify();
+            }
         }
     }
 
     static class Consumer {
 
         void consume() {
-            while (isEmpty(buffer)) {}
-            buffer[--count] = 0;
-            printBuffer("Consume");
+            synchronized (lock) {
+                if (isEmpty(buffer)) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                buffer[--count] = 0;
+                printBuffer("Consume");
+                lock.notify();
+            }
         }
     }
 
@@ -32,11 +54,11 @@ public class ProducerConsumer {
     }
 
     static void printBuffer(String operation) {
-        // System.out.println(operation + "<<<");
-        // for (int v: buffer) {
-        //     System.out.print(v);
-        // }
-        // System.out.println(">>>");
+        System.out.println(operation + "<<<");
+        for (int v: buffer) {
+            System.out.print(v);
+        }
+        System.out.println(">>>");
     }
 
     public static void main(String... strings) throws InterruptedException {
@@ -54,9 +76,14 @@ public class ProducerConsumer {
             System.out.println("Done producing");
         };
         Runnable consumeTask = () -> {
-            for (int i = 0; i < 50; i++) {
+            for (int i = 0; i < 45; i++) {
                 consumer.consume();
-            }
+                try {
+                    Thread.sleep(10); // slow down consumer
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+        }
             System.out.println("Done consuming");
         };
 
