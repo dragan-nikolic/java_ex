ef6e99945b63c85218ef0c231e025c2f5b9bdd29
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2019-12-15 14:36:54 +0100

    complete lucky ticket

--- courses/gridu_school/l1/LuckyNumber/src/main/java/gd/belgrade/school/LuckyNumber.java
+++ courses/gridu_school/l1/LuckyNumber/src/main/java/gd/belgrade/school/LuckyNumber.java
@@ -3,6 +3,8 @@ package gd.belgrade.school;
 import java.util.Scanner;
 
 public class LuckyNumber {
+    final static int LN_LENGTH = 6;
+
     /**
      * 1. Ticket number is an input of the method
      * 2. Valid ticket number (or ticket id) has length of 6 (always)
@@ -14,19 +16,44 @@ public class LuckyNumber {
      *  a. 123456 -> false | 1+2+3 != 4+5+6
      *  b. 123321 -> true  | 1+2+3 == 3+2+1 
      */
-    public static boolean isMyTicketLucky(String ticketNumber) {
-        return false;
+    public static boolean isMyTicketLucky(String ticketNumber) throws Exception {
+        if (ticketNumber.length() != LN_LENGTH) {
+            throw new Exception("Invalid ticket " + ticketNumber + "! Length must be " + LN_LENGTH);
+        }
+
+        for (char c: ticketNumber.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                throw new Exception("Invalid ticket " + ticketNumber + "! Ticket number must have digits only.");
+            }
+        }
+
+        int fh = 0;
+        int sh = 0;
+        for (int i=0; i<LN_LENGTH/2; i++) {
+            fh += Character.getNumericValue(ticketNumber.charAt(i));
+            sh += Character.getNumericValue(ticketNumber.charAt(i+LN_LENGTH/2));
+        }
+
+        return (fh == sh);
     }
 
-    static Scanner in;
     public static void main(String[] args) {
-        in = new Scanner(System.in);
-        System.out.print("Please enter: ");
+        Scanner in = new Scanner(System.in);
+        String ticketNumber = "";
 
-        String s1 = in.nextLine();
+        do {
+            System.out.print("Please enter: ");
+            ticketNumber = in.nextLine();
 
-        System.out.println("Entered: " + s1);
+            try {
+                boolean result = isMyTicketLucky(ticketNumber);
+                System.out.println("Ticket " + ticketNumber + " is " + result);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        } while(!ticketNumber.isEmpty());
 
+        System.out.println("Goodbye!");
         in.close();
     }
 }
