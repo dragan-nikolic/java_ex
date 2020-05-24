558e85437b5d4b5db9d786c057f70d6f49ff86ef
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2020-05-24 20:34:43 +0200

    autowire using constructor insterad of setter

--- courses/pluralsight/spring_fundamentals/conference-java/src/main/java/org/dragan/service/SpeakerServiceImpl.java
+++ courses/pluralsight/spring_fundamentals/conference-java/src/main/java/org/dragan/service/SpeakerServiceImpl.java
@@ -16,6 +16,7 @@ public class SpeakerServiceImpl implements SpeakerService {
         System.out.println("SpeakerServiceImpl no args constructor");
     }
 
+    @Autowired
     public SpeakerServiceImpl(SpeakerRepository repository) {
         System.out.println("SpeakerServiceImpl repository constructor");
         this.repository = repository;
@@ -26,7 +27,6 @@ public class SpeakerServiceImpl implements SpeakerService {
         return repository.findAll();
     }
 
-    @Autowired
     public void setRepository(SpeakerRepository repository) {
         System.out.println("SpeakerServiceImpl setter");
         this.repository = repository;
