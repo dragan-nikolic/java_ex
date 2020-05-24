1ac91dc87aceaaa00d425e5b47e0e5ace32cc330
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2020-05-24 18:57:19 +0200

    complete spring scope

--- courses/pluralsight/spring_fundamentals/conference-java/src/main/java/Application.java
+++ courses/pluralsight/spring_fundamentals/conference-java/src/main/java/Application.java
@@ -7,8 +7,12 @@ import org.springframework.core.env.SystemEnvironmentPropertySource;
 public class Application {
     public static void main(String args[]) {
         ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
         SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
+        System.out.println(service);
 
         System.out.println(service.findAll().get(0).getFirstName());
+        SpeakerService service2 = appContext.getBean("speakerService", SpeakerService.class);
+        System.out.println(service2);
     }
 }
