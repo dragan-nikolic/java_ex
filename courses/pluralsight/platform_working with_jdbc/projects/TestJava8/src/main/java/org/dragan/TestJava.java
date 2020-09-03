d274ecff9b2e70f12cfa09295e291a4a5833d530
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2020-09-03 23:13:09 +0200

    add jdbc project to test if java8 is ok

---
+++ courses/pluralsight/platform_working with_jdbc/projects/TestJava8/src/main/java/org/dragan/TestJava.java
@@ -0,0 +1,7 @@
+package org.dragan;
+
+public class TestJava {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Platform: Working with databases using JDBC!");
+    }
+}
