5c9a732ec438c8407d52dc82464f0070b7dabbfe
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2020-09-18 14:53:04 +0200

    jwf - init

---
+++ courses/pluralsight/java_web_fundamentals/README.md
@@ -0,0 +1,5 @@
+# [Java Web Fundamentals](https://app.pluralsight.com/library/courses/java-web-fundamentals/table-of-contents)
+
+## Setup Tomcat
+
+Download and install v7 from Tomcat download.
---
+++ courses/pluralsight/java_web_fundamentals/projects/SimpleServlet/src/main/java/org/dragan/SimpleServlet.java
@@ -0,0 +1,21 @@
+package org.dragan;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SimpleServlet extends HttpServlet {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.getWriter().write("<h2>Hello World</h2>");
+    }
+}
