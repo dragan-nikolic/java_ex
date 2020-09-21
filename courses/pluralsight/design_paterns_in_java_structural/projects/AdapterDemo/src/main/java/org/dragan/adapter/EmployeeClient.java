a57bf14094835fef9241543aeff5cd34e8638e81
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2020-09-21 22:40:48 +0200

    add another adapter

--- courses/pluralsight/design_paterns_in_java_structural/projects/AdapterDemo/src/main/java/org/dragan/adapter/EmployeeClient.java
+++ courses/pluralsight/design_paterns_in_java_structural/projects/AdapterDemo/src/main/java/org/dragan/adapter/EmployeeClient.java
@@ -19,6 +19,10 @@ public class EmployeeClient {
 
         employees.add(new EmployeeAdapterLdap(employeeFromLdap));
 
+        EmployeeCSV employeeFromCSV = new EmployeeCSV("567,Sherlock,Holmes,sherlock@holmes.com");
+
+        employees.add(new EmployeeAdapterCSV(employeeFromCSV));
+
         return employees;
     }
 }
