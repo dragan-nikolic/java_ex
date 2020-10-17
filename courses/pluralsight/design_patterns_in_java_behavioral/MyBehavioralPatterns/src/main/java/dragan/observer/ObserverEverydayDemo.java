0a2283ef6a1d30f7ffd70e5fe57aca753b4e7af7
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2020-10-17 15:17:57 +0200

    observer pattern

---
+++ courses/pluralsight/design_patterns_in_java_behavioral/MyBehavioralPatterns/src/main/java/dragan/observer/ObserverEverydayDemo.java
@@ -0,0 +1,45 @@
+package dragan.observer;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class ObserverEverydayDemo {
+
+    public static void main(String args[]) {
+        TwitterStream messageStream = new TwitterStream();
+
+        Client client1 = new Client("Bryan");
+
+        Client client2 = new Client("Mark");
+
+        messageStream.addObserver(client1);
+        messageStream.addObserver(client2);
+
+        messageStream.someoneTweeted();
+    }
+}
+
+// concrete subject
+class TwitterStream extends Observable {
+
+    public void someoneTweeted() {
+        setChanged();
+        notifyObservers("info to observers");
+    }
+}
+
+//concrete observer
+class Client implements Observer {
+    private String name;
+
+    Client(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        System.out.println("Update " + name + "'s stream, someone tweeted something.");
+        System.out.println(o);
+        System.out.println(arg);
+    }
+}
