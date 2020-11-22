af70508a146c82db4846cdda07b6973416cbf74b
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2020-11-22 09:48:18 +0100

    addGoal work :)

--- courses/pluralsight/spring_mvc_intro/my_exercises/FitnessTracker/src/main/java/com/dragan/controller/MinutesController.java
+++ courses/pluralsight/spring_mvc_intro/my_exercises/FitnessTracker/src/main/java/com/dragan/controller/MinutesController.java
@@ -13,14 +13,6 @@ public class MinutesController {
 	public String addMinutes(@ModelAttribute("exercise") Exercise exercise) {
 		System.out.println("exercise: " + exercise.getMinutes());
 
-		//return "forward:addMoreMinutes.html";
-		return "redirect:addMoreMinutes.html";
-	}
-
-	@RequestMapping(value = "/addMoreMinutes")
-	public String addMoreMinutes(@ModelAttribute("exercise") Exercise exercise) {
-		System.out.println("exercising: " + exercise.getMinutes());
-
 		return "addMinutes";
 	}
 }
