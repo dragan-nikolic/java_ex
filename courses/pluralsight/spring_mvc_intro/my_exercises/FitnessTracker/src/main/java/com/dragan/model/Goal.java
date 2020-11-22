bb0ecc1c550af29ee1f405f4d169ce5b62c90756
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2020-11-22 13:15:03 +0100

    add validation

--- courses/pluralsight/spring_mvc_intro/my_exercises/FitnessTracker/src/main/java/com/dragan/model/Goal.java
+++ courses/pluralsight/spring_mvc_intro/my_exercises/FitnessTracker/src/main/java/com/dragan/model/Goal.java
@@ -1,7 +1,10 @@
 package com.dragan.model;
 
+import org.hibernate.validator.constraints.Range;
+
 public class Goal {
 
+	@Range(min=1, max=120)
 	private int minutes;
 
 	public int getMinutes() {
