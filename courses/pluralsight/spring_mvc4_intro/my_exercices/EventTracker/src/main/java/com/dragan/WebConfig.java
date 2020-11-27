3f1149ed186ca810c84257901b9b04ebc2159139
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2020-11-27 20:55:22 +0100

    hello controller with web.xml DOES NOT WORK :(

--- courses/pluralsight/spring_mvc4_intro/my_exercices/EventTracker/src/main/java/com/dragan/WebConfig.java
+++ courses/pluralsight/spring_mvc4_intro/my_exercices/EventTracker/src/main/java/com/dragan/WebConfig.java
@@ -1,10 +1,12 @@
 package com.dragan;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
+@ComponentScan(basePackages = "com.dragan")
 public class WebConfig {
 
 }
--- courses/pluralsight/spring_mvc4_intro/my_exercices/EventTracker/src/main/java/com/dragan/controller/HelloController.java
+++ courses/pluralsight/spring_mvc4_intro/my_exercices/EventTracker/src/main/java/com/dragan/controller/HelloController.java
@@ -3,14 +3,15 @@ package com.dragan.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HelloController {
 
-	@RequestMapping(value="/greeting")
+	@RequestMapping(value="/greeting", method=RequestMethod.GET)
 	public String sayHello(Model model) {
 		model.addAttribute("greeting", "Hello World");
 		
-		return "hello";
+		return "hello.jsp";
 	}
 }
