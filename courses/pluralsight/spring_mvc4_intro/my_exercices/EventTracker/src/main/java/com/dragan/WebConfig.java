fa4e46917737973a2ee760d7b294465748266d4f
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2020-11-28 18:45:57 +0100

    add pdf support

--- courses/pluralsight/spring_mvc4_intro/my_exercices/EventTracker/src/main/java/com/dragan/WebConfig.java
+++ courses/pluralsight/spring_mvc4_intro/my_exercices/EventTracker/src/main/java/com/dragan/WebConfig.java
@@ -4,18 +4,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.dragan")
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter {
 	@Bean
 	public InternalResourceViewResolver getInternalResourceViewResolver() {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setPrefix("/views/");
+		resolver.setPrefix("/WEB-INF/views/");
 		resolver.setSuffix(".jsp");
 		
 		return resolver;
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/pdfs/**").addResourceLocations("/WEB-INF/documents/");
 	}
 }
