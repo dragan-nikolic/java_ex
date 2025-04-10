1b6a6dfeb79c35e02791a460a27dff8228763843
Dragan Nikolic <dragan.nikolic@griddynamics.com>
2025-04-10 13:24:05 +0200

    update TAU rest assured course

--- tutorials/rest-assured/tau-rest-assured/tau-rest-assured-project/src/test/java/examples/Chapter1Test.java
+++ tutorials/rest-assured/tau-rest-assured/tau-rest-assured-project/src/test/java/examples/Chapter1Test.java
@@ -10,21 +10,42 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class Chapter1Test {
 
+    /**
+     * Request: curl --request GET --url https://api.zippopotam.us/US/90210
+     *
+     * Response:
+     * {
+     *   "post code": "90210",
+     *   "country": "United States",
+     *   "country abbreviation": "US",
+     *   "places": [
+     *     {
+     *       "place name": "Beverly Hills",
+     *       "longitude": "-118.4065",
+     *       "state": "California",
+     *       "state abbreviation": "CA",
+     *       "latitude": "34.0901"
+     *     }
+     *   ]
+     * }
+     */
     @Test
     public void requestUsZipCode90210_checkPlaceNameInResponseBody_expectBeverlyHills() {
-
-        // RequestSpecification xgiven = given();
-        // RequestSpecification xwhen = xgiven.when();
-        // Response xget = xwhen.get("http://zippopotam.us/us/90210");
-        // ValidatableResponse xthen = xget.then();
-        // ValidatableResponse xassert = xthen.assertThat();
-        // ValidatableResponse xbody = xassert.body("places[0].'place name'", equalTo("Beverly Hills"));
-        // int x = 5;
        given().
        when().
            get("http://zippopotam.us/us/90210").
        then().
            assertThat().
            body("places[0].'place name'", equalTo("Beverly Hills"));
+    }
+
+    @Test
+    public void requestUsZipCode90210_checkPlaceNameInResponseBody_expectBeverlyHills_v2() {
+         RequestSpecification xgiven = given();
+         RequestSpecification xwhen = xgiven.when();
+         Response xget = xwhen.get("http://zippopotam.us/us/90210");
+         ValidatableResponse xthen = xget.then();
+         ValidatableResponse xassert = xthen.assertThat();
+         xassert.body("places[0].'place name'", equalTo("Beverly Hills"));
     }
 }
