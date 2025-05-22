1f0bc7705b160597695c940fa8294a48290132bd
Dragan Nikolic <dragan.nikolic@griddynamics.com>
2025-05-22 17:25:21 +0200

    use junit 5 parametrized test

--- tutorials/rest-assured/tau-rest-assured/tau-rest-assured-project/src/test/java/examples/Chapter3Test.java
+++ tutorials/rest-assured/tau-rest-assured/tau-rest-assured-project/src/test/java/examples/Chapter3Test.java
@@ -1,32 +1,26 @@
-// use https://www.baeldung.com/parameterized-tests-junit-5
+// use JUnit 5 ParameterizedTest instead of JUnit 4 DataProvider
 
 package examples;
 
-import com.tngtech.java.junit.dataprovider.*;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.runner.RunWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
+import java.util.stream.Stream;
 
-@RunWith(DataProviderRunner.class)
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
 public class Chapter3Test {
 
-    @DataProvider
-    public static Object[][] zipCodesAndPlaces() {
-        return new Object[][] {
-            { "us", "90210", "Beverly Hills" },
-            { "us", "12345", "Schenectady" },
-            { "ca", "B2R", "Waverley"}
-        };
-    }
-
-    @Test
-    @UseDataProvider("zipCodesAndPlaces")
+    @ParameterizedTest
+    @MethodSource("zipCodesAndPlaces")
     public void requestZipCodesFromCollection_checkPlaceNameInResponseBody_expectSpecifiedPlaceName(String countryCode, String zipCode, String expectedPlaceName) {
 
         given().
-            pathParam("countryCode", countryCode).pathParam("zipCode", zipCode).
+            pathParam("countryCode", countryCode).
+            pathParam("zipCode", zipCode).
         when().
             get("http://zippopotam.us/{countryCode}/{zipCode}").
         then().
@@ -34,36 +28,11 @@ public class Chapter3Test {
             body("places[0].'place name'", equalTo(expectedPlaceName));
     }
 
-    @Test
-    public void requestUsZipCode90210_checkPlaceNameInResponseBody_expectBeverlyHills() {
-
-        given().
-        when().
-            get("http://zippopotam.us/us/90210").
-        then().
-            assertThat().
-            body("places[0].'place name'", equalTo("Beverly Hills"));
-    }
-
-    @Test
-    public void requestUsZipCode12345_checkPlaceNameInResponseBody_expectSchenectady() {
-
-        given().
-        when().
-            get("http://zippopotam.us/us/12345").
-        then().
-            assertThat().
-            body("places[0].'place name'", equalTo("Schenectady"));
-    }
-
-    @Test
-    public void requestCaZipCodeB2R_checkPlaceNameInResponseBody_expectWaverley() {
-
-        given().
-        when().
-            get("http://zippopotam.us/ca/B2R").
-        then().
-            assertThat().
-            body("places[0].'place name'", equalTo("Waverley"));
+    static Stream<Arguments> zipCodesAndPlaces() {
+        return Stream.of(
+                arguments("us", "90210", "Beverly Hills"),
+                arguments("us", "12345", "Schenectady"),
+                arguments("ca", "B2R", "Waverley")
+        );
     }
 }
